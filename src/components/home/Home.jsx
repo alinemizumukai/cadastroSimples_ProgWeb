aaee9ed912d384686bb6da57f52ad8aa8b27baa1
Aline Mizumukai <alinemizumukai@yahoo.com.br>
2022-02-20 17:15:18 -0300

    Commit inicial

---
+++ src/components/home/Home.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import Main from '../templates/Main'
+
+export default props =>
+    <Main icon="home" title="Inicio" subtitle="Projeto crud">
+        <div className='display-4'>Bem Vindo!</div>
+        <hr />
+        <p className="mb-0">Sistema de Cadastro</p>
+    </Main>
